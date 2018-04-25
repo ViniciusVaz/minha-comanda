edf30f37c6f77fad48c4ea4cf97cef7ef0ac159f
Vinicius <viniciushenriquevaz@hotmail.com>
2018-04-25 18:11:07 -0300

    - Added validations - Added payment options - Created vuex actions and getters

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import jQuery from 'jquery';
+
+import "_styles/main.scss"
 
 import App from "_components/App.vue"
 import store from "_vuex/store"
 
+
+window.jQuery = jQuery;
+window.$ = jQuery;
 
 new Vue({
     ...App,
